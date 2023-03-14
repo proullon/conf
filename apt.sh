eff0801dd00c179811fc59bf6212287c6ec23d42
Pierre Roullon <pierre.roullon@gmail.com>
2023-03-14 18:13:54 +0100

    fix: correct apt.sh

--- apt.sh
+++ apt.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 apt-get update
-apt-get upgrde -y
-apt-get install nvim cmake gettext latexmk
-cd ~/work/src; mkdir -p github.com/neovim; cd github.com/neovim; git clone git@github.com:neovim/neovim.git; cd neovim; make CMAKE_BUILD_TYPE=RelWithDebInfo; make install
+apt-get upgrade -y
+apt-get install nvim cmake gettext latexmk python3 python3-pip
+python3 -m pip install --user --upgrade pynvim
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd ~/work/src; mkdir -p github.com/neovim; cd github.com/neovim; git clone git@github.com:neovim/neovim.git; cd neovim; make CMAKE_BUILD_TYPE=RelWithDebInfo; sudo make install
 nvim -v
