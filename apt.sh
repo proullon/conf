40eeb78f3f2b428fd293a7f389bf5f9c2a89a5b3
Pierre Roullon <pierre.roullon@gmail.com>
2023-05-04 09:13:41 +0200

    feat: add vim-plug setup

--- apt.sh
+++ apt.sh
@@ -22,3 +22,6 @@ curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-di
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd ~/work/src; mkdir -p github.com/neovim; cd github.com/neovim; git clone git@github.com:neovim/neovim.git; cd neovim; make CMAKE_BUILD_TYPE=RelWithDebInfo; sudo make install
 nvim -v
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
