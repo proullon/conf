81a5903780103a6b5174862f1d52baef8e0d6f1d
Pierre Roullon <pierre.roullon@gmail.com>
2025-12-03 16:24:11 +0100

    feat(nvim-cmp): capture arrow on completion list

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,69 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        -- Trigger completion
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        -- Validate selection
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+        -- FIX: allow arrow keys to navigate the completion menu
+        ["<Down>"] = cmp.mapping.select_next_item({
+          behavior = cmp.SelectBehavior.Select,
+        }),
+        ["<Up>"] = cmp.mapping.select_prev_item({
+          behavior = cmp.SelectBehavior.Select,
+        }),
+
+        -- Tab navigation (fallback-safe)
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
+
