780c5e5af7b38a19a1c22cd3b01b74435a87deaf
Pierre Roullon <pierre.roullon@gmail.com>
2025-12-03 17:21:37 +0100

    fix(cmp): autocomplete on buffer only if keywork length superior to 3

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -60,7 +60,7 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "buffer" },
+        { name = "buffer", keyword_length = 3},
         { name = "path" },
       }),
     })
