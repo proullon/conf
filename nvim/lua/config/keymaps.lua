add6a7f465f796dd88c808f15450a409bba91099
Pierre Roullon <pierre.roullon@gmail.com>
2025-12-26 21:47:50 +0100

    feat(nvim): add shortcut for strudel

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -36,9 +36,20 @@ vim.api.nvim_create_user_command("GPT", function(cmd_opts)
     print(result)
 end, { nargs = 1, complete = "file" })
 
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
 -- Spelling correction of word bellow cursor
 map("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", opts)
 
+-- Strudel specific keymaps
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "javascript",
+    callback = function(ev)
+        local strudel = require("strudel")
+        vim.keymap.set("n", "<leader>sl", strudel.launch, { desc = "Launch Strudel" })
+        vim.keymap.set("n", "<leader>sq", strudel.quit, { desc = "Quit Strudel" })
+        vim.keymap.set("n", "<leader>st", strudel.toggle, { desc = "Strudel Toggle Play/Stop" })
+        vim.keymap.set("n", "<leader>su", strudel.update, { desc = "Strudel Update" })
+        vim.keymap.set("n", "<leader>ss", strudel.stop, { desc = "Strudel Stop Playback" })
+        vim.keymap.set("n", "<leader>sb", strudel.set_buffer, { desc = "Strudel set current buffer" })
+        vim.keymap.set("n", "<leader>sx", strudel.execute, { desc = "Strudel set current buffer and update" })
+    end,
+})
--- nvim/lua/plugins/strudel.lua
+++ nvim/lua/plugins/strudel.lua
@@ -1,11 +1,13 @@
 return {
-{
-  "gruvw/strudel.nvim",
-  build = "npm ci",
-  config = function()
-    require("strudel").setup({
-      -- tes options ici (voir plus bas)
-    })
-  end,
-},
+    {
+        "gruvw/strudel.nvim",
+        build = "npm ci",
+        config = function()
+            require("strudel").setup({
+                -- Headless mode: set to `true` to run the browser without launching a window
+                -- (optional, default: false)
+                headless = true,
+            })
+        end,
+    },
 }
